1ef1e839979e7775da053e48bc02b3d55a45b17a
Knut Haugen <knut.haugen@nrk.no>
2019-12-03 07:50:11 +0100

    template file

---
+++ src/template.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+
+function main(filename) {
+  fs.readFile('./' + filename, (_, data) => {
+    const lines = data.toString().split('\n')
+    const list = lines[0].split(',').map((i) => {
+      return parseInt(i)
+    })
+  })
+}
+
+module.exports.main = main
