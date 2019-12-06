0f4420bc432b57f9039dcf2a154fabbcf7bbb794
Knut Haugen <knut.haugen@nrk.no>
2019-12-06 19:53:12 +0100

    fixed

--- src/day6.js
+++ src/day6.js
@@ -13,16 +13,14 @@ function sumOrbits(orbits) {
     const [center, orbiter] = orbits[i].split(')')
     paths[orbiter] = center
   }
-
+  delete(paths['undefined'])
   let orbitCount = 0
   let current
 
   const keys = Object.keys(paths)
 
   for(let i = 0; i < keys.length; i++) {
-    if(keys[i] === 'undefined') {
-      break;
-    }
+
     const key = keys[i]
     current = paths[key]
     if(current === 'COM') {
@@ -36,7 +34,39 @@ function sumOrbits(orbits) {
     }
     orbitCount++
   }
+
+  console.log(`shortest path: ${findShortestPath(paths, 'YOU', 'SAN')}`)
+
   return orbitCount
+}
+
+function iterate(paths, start) {
+  const result = []
+  let current = paths[start]
+  
+  while(current !== 'COM') {
+    result.push(current)
+    current = paths[current]
+  }
+  return result
+}
+
+function findShortestPath(paths, start, end) {
+  const startPath = iterate(paths, start)
+  let endPath = iterate(paths, end)
+
+  //find index of shortest common ancestor
+  let commonAncestorIndex = 0
+  for(let i = startPath.length; i> 0; i--) {
+    const found = endPath.findIndex(x => x === startPath[i])
+    if(found !== -1) {
+      commonAncestorIndex = found
+    }
+  }
+  const secondindex = startPath.findIndex(x => x === endPath[commonAncestorIndex])
+  //console.log(commonAncestorIndex + secondindex)
+  return commonAncestorIndex + secondindex
+
 }
 
 module.exports.sumOrbits = sumOrbits
